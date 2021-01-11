bf99f8484406def0025f382dcdbd22f54f999885
unknown <Leonardo>
2021-01-10 21:06:09 -0500

    2da Version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import { Container } from "react-bootstrap";
+import HomePage from "./pages/HomePage";
+import ProductPage from "./pages/ProductPage";
+import LoginPage from "./pages/LoginPage";
+import CartPage from "./pages/CartPage";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <Route path="/" component={HomePage} exact></Route>
+          <Route path="/product/:id" component={ProductPage} exact></Route>
+          <Route path="/login" component={LoginPage} exact></Route>
+          <Route path="/cart" component={CartPage} exact></Route>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
---
+++ src/pages/CartPage.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function CartPages() {
+  return <div>Cart</div>;
+}
